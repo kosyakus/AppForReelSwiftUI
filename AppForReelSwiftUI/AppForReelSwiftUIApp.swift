5e09036c817bcf6e24807c8c0bb6e67feedd880f
Natali <ns@numbuster.com>
2024-08-05 14:56:05 +0300

    Initial Commit

---
+++ AppForReelSwiftUI/AppForReelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForReelSwiftUIApp.swift
+//  AppForReelSwiftUI
+//
+//  Created by Natalia Sinitsyna on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppForReelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
